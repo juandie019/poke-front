83c95d3b364e52762eb79db166cecc329dd46a37
Juan Diego <juandie019@gmail.com>
2024-01-18 20:29:47 -0600

    ADD: pokemon drawer

--- src/components/PokemonDrawer.jsx
+++ src/components/PokemonDrawer.jsx
@@ -1,11 +1,18 @@
 import { PokemonLink } from "./PokemonLink";
-export const PokemonDrawer = ({ pokemons = [] }) => {
+
+export const PokemonDrawer = ({ pokemons = [], loading = false, onNext, onPrev, nextDisabled, prevDisabled, onPokemonClick }) => {
     return (
         <>
             <nav className="drawer">
+                <div className="row space-between">
+                    <button className="main-button" disabled={prevDisabled}  onClick={onPrev}>Prev</button>
+                    <button className="main-button" disabled={nextDisabled}  onClick={onNext}>Next</button>
+                </div>
                 <ul>
-                    {
-                        pokemons.map(({ name, image, id }) => <li  key={id}><PokemonLink name={name} image={image} id={id}/></li>)
+                    { 
+                        !loading ?
+                        pokemons.map(({ name, image, id }) => <li key={id} onClick={() => onPokemonClick(id)} ><PokemonLink name={name} image={image} id={id}/></li>) :
+                        <p className="padding">Loading...</p>
                     }
                 </ul>
             </nav>
