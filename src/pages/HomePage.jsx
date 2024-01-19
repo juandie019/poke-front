71994647f08e9da1846b4218a8c13664f0dff2cd
Juan Diego <juandie019@gmail.com>
2024-01-18 20:54:40 -0600

    ADD: home page

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,16 +1,62 @@
+import { useEffect, useState } from 'react';
+import { getPokemons, getPokemon } from '../services/pokemonService'
 import { PokemonDrawer } from '../components/PokemonDrawer'
+import { PokemonCard } from '../components/PokemonCard';
 
 export const HomePage = () => {
-    const pokemons = [{id: 1, name: 'bulbasur', image: 'https://unpkg.com/pokeapi-sprites@2.0.2/sprites/pokemon/other/dream-world/63.svg'}, {id: 2, name: 'Ivy', image: 'https://unpkg.com/pokeapi-sprites@2.0.2/sprites/pokemon/other/dream-world/142.svg'}]
+    const [pokemons, setPokemons] = useState([]);
+    const [chosenPokemon, setChosenPokemon] = useState(null);
+
+    const [pagintation, setPagination] = useState({ page: 0, next: false, previous: false });
+    
+    const [loadingPokemons, setLoadingPokemons] = useState(false);
+    const [changePage, setChangePage] = useState(0);
+    const [searchTerm, setSearchTerm] = useState('');
+
+    const fetchPokemons = async (page) => {
+        setLoadingPokemons(true);
+        
+        const pokemonsData = await getPokemons(page, searchTerm);
+        setPokemons(pokemonsData.pokemons);
+        setPagination(pokemonsData.pagination); 
+
+        setLoadingPokemons(false);
+    }
+
+    const fetchPokemon = async (nameOrId) => {
+        const pokemon = await getPokemon(nameOrId);
+        setChosenPokemon(pokemon);
+    }
+
+    const downloadPokemon = async () => {
+        console.log('dowload');
+    }
+
+    useEffect(() => {
+        fetchPokemons(changePage);
+    }, [changePage, searchTerm])
 
     return (
         <>
             <div className="row">
                 {/* left side */}
-                <PokemonDrawer pokemons={pokemons}/>
+                <PokemonDrawer 
+                    pokemons={pokemons} 
+                    loading={loadingPokemons}
+                    nextDisabled={!pagintation.next || loadingPokemons}
+                    prevDisabled={!pagintation.previous || loadingPokemons}
+                    onNext={() => setChangePage(pagintation.page + 1)}
+                    onPrev={() => setChangePage(pagintation.page - 1)}
+                    onPokemonClick={fetchPokemon}
+                />
 
                 {/* right side */}
-                <p>content</p>
+                <div>
+                    <input className='margin-md padding' placeholder='Search' value={searchTerm} onChange={e => setSearchTerm(e.target.value)}></input>
+                    {
+                        chosenPokemon && <PokemonCard pokemon={chosenPokemon} onCatchPokemon={downloadPokemon}/> 
+                    }
+                </div>
             </div>
         </>
     )
